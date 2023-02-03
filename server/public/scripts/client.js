726fab50cbb5aa3950723c590dd7cdf4487f1281
CJ Simon <cjsimon250@outlook.com>
2023-02-03 00:24:57 -0600

    finished wireframe

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,40 @@ $(document).ready(onReady);
 
 function onReady() {
   render();
+  $("#create-btn").on("click", toggleCreateButtons);
 }
 
-function render() {}
+let showCreateInputs = false;
+
+//toggle create buttons
+function toggleCreateButtons() {
+  if (showCreateInputs === true) {
+    showCreateInputs = false;
+  } else {
+    showCreateInputs = true;
+  }
+  render();
+}
+
+//display new color options for todo
+function showNewToDoColors() {
+  $(".color-btns").empty();
+
+  $(".color-btns").append(`
+    <button class="create-input-btns" id="yellow-btn"></button>
+    <button class="create-input-btns" id="orange-btn"></button>
+    <button class="create-input-btns" id="purple-btn"></button>
+    <button class="create-input-btns" id="blue-btn"></button>
+    <button class="create-input-btns" id="green-btn"></button>
+    `);
+}
+
+function render() {
+  if (showCreateInputs === false) {
+    $(".color-btns").empty();
+    $("#create-btn").text("+");
+  } else {
+    showNewToDoColors();
+    $("#create-btn").text("^");
+  }
+}
